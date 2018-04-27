660ffcc10ba597350616cdbd888b90edc9a81e48
maksim-maslov <brat.maksim@gmail.com>
2018-04-28 00:28:07 +0300

    Добавил решение задания

--- index.js
+++ index.js
@@ -12,30 +12,33 @@ const url = 'mongodb://localhost:27017/test';
 mongoose.connect(url);
 
 const userSchema = new mongoose.Schema({
-	name: String
+  name: String
 });
 
 const taskSchema = new mongoose.Schema({
-	name: String,
-	description: String,
-	status: String,
-	user: String
+  name: String,
+  description: String,
+  status: String,
+  user: String
 });
 
 
+// Users
+//
+
 app.post('/users/', (req, res) => {
 
-	mongoose.connect(url);
+  mongoose.connect(url);
 
-	const User = mongoose.model('User', userSchema);
+  const User = mongoose.model('User', userSchema);
 
-	User.create({name: req.body.name}, (err, result) => {
-		if (err) {
-			console.log(err);
-		} else {
-			res.send(result);
-		}
-	});
+  User.create({name: req.body.name}, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
 
 });
 
@@ -43,67 +46,164 @@ app.post('/users/', (req, res) => {
 
 app.get('/users/', (req, res) => { 
 
-	mongoose.connect(url);
+  mongoose.connect(url);
 
-  if (req.query.q) {
+  const User = mongoose.model('User', userSchema); 
 
-		const User = mongoose.model('User', userSchema); 
-
-		const regex = `.*${req.query.q}.*`;
-
-		User.find({name: {$regex: regex}}, (err, result) => {
-			res.send(result);
-		});
-
-  } else {
-
-		const User = mongoose.model('User', userSchema); 
-
-		User.find((err, result) => {
-			res.send(result);
-		});
-
-  }    
+  User.find((err, result) => {
+    if (err) {
+      console.log(err);
+    } else if (result.length) {
+      res.json(result);
+    } else {
+      res.send('Нет документов с данным условием поиска');
+    }
+  });
+   
 });
 
 
 
 app.put('/users/:id', (req, res) => {
 
-	mongoose.connect(url);
+  mongoose.connect(url);
 
-	const User = mongoose.model('User', userSchema);
+  const User = mongoose.model('User', userSchema);
 
-	const ObjectId = mongoose.Types.ObjectId;
+  const ObjectId = mongoose.Types.ObjectId;
   const id = new ObjectId(req.params.id);    
 
-	User.update({_id: id}, {$set: {name: req.body.name}}, (err, result) => {
-		if (err) {
-			console.log(err);
-		} else {
-			res.send(result);
-		}		
-	});
+  User.update({_id: id}, {$set: {name: req.body.name}}, (err, result) => {
+
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }    
+  });
 });
 
 
 
 app.delete('/users/:id', (req, res) => {
 
-	mongoose.connect(url);
+  mongoose.connect(url);
 
-	const User = mongoose.model('User', userSchema);
+  const User = mongoose.model('User', userSchema);
 
-	const ObjectId = mongoose.Types.ObjectId;
+  const ObjectId = mongoose.Types.ObjectId;
   const id = new ObjectId(req.params.id);    
 
-	User.remove({_id: id}, (err, result) => {
-		if (err) {
-			console.log(err);
-		} else {
-			res.send(result);
-		}		
-	});
+  User.remove({_id: id}, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }    
+  });
+});
+
+
+
+// Tasks
+//
+
+app.post('/tasks/', (req, res) => {
+
+  mongoose.connect(url);
+
+  const Task = mongoose.model('Task', taskSchema);
+
+  Task.create({
+    name: req.body.name,
+    description: req.body.description,
+    status: req.body.status,
+    user: req.body.user
+  }, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+
+});
+
+
+
+app.get('/tasks/', (req, res) => { 
+
+  mongoose.connect(url);
+
+  const Task = mongoose.model('Task', taskSchema);
+
+  if (req.query.q) {
+
+    const regex = `.*${req.query.q}.*`;
+
+    Task.find({$or: [{name: {$regex: regex}}, {description: {$regex: regex}}]}, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else if (result.length) {
+        res.json(result);
+      } else {
+        res.send('Нет документов с данным условием поиска');
+      }
+    });
+
+  } else {
+
+    Task.find((err, result) => {
+      if (err) {
+        console.log(err);
+      } else if (result.length) {
+        res.json(result);
+      } else {
+        res.send('Нет документов с данным условием поиска');
+      }
+    });
+
+  }    
+});
+
+
+
+app.put('/tasks/:id', (req, res) => {
+
+  mongoose.connect(url);
+
+  const Task = mongoose.model('Task', taskSchema); 
+
+  const ObjectId = mongoose.Types.ObjectId;
+  const id = new ObjectId(req.params.id);    
+
+  Task.update({_id: id}, {$set: {status: req.body.status, user: req.body.user}}, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }    
+  });
+});
+
+
+
+app.delete('/tasks/:id', (req, res) => {
+
+  mongoose.connect(url);
+
+  const Task = mongoose.model('Task', taskSchema); 
+
+  const ObjectId = mongoose.Types.ObjectId;
+  const id = new ObjectId(req.params.id);    
+
+  Task.remove({_id: id}, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }    
+  });
 });
 
 
